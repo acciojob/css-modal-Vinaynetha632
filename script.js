a7e9265e5d11fd6a88f68d1f87f6993d875a5c17
Vinaynetha632 <thiramdasuvinay@gmail.com>
2026-02-08 12:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-modal");
+
+openBtn.onclick = () =>{
+modal.style.display = "flex";
+};
+closeBtn.onclick = () =>{
+modal.style.display = "none";
+};
+window.onclick = (event) => {
+	if(event.target === modal){
+		modal.style.display = "none";
+	}
+};
